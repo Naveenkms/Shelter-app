ab51dea00e3d46cf0c8c940418f2c5d58fb96a8f
Naveen Krishna M S <nvnkms123@gmail.com>
2022-10-16 14:11:32 +0530

    add book.js

--- pages/book.js
+++ pages/book.js
@@ -1,14 +1,14 @@
-import { useState } from "react"
+
 import Header from "../components/Header"
-import InfoCard from "../components/InfoCard"
+
 
 const book = () => {
-    const [book, setBook] = useState(false)
+    
   return (
     <div>
         <Header />
         
-        {book ? <InfoCard /> : <h1>Not booked anything</h1>}
+        <h1>Not booked anything</h1>
     </div>
   
   )
